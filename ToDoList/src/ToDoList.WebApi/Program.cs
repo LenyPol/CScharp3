7299edb7b26fbdafcd511630acf86517a53abd20
LenyPol <lenkapolova37@gmail.com>
2025-10-07 21:00:48 +0200

    Vyřešený merge konflikt v Program.cs

--- ToDoList/src/ToDoList.WebApi/Program.cs
+++ ToDoList/src/ToDoList.WebApi/Program.cs
@@ -4,19 +4,8 @@ var builder = WebApplication.CreateBuilder(args);
     builder.Services.AddControllers();
 }
 var app = builder.Build();
-<<<<<<< HEAD
 {
-    // Configure Middlewre (Http request pipeline)
+    // Configure Middleware (HTTP request pipeline)
     app.MapControllers();
 }
-=======
-
-app.MapGet("/", () => "Hello World!");
-app.MapGet("/test", () => "This is a test!");
-app.MapGet("/czechitas", () => "Vítej na kurzu Czechitas!");
-app.MapGet("/pozdrav/{jmeno}", (string jmeno) => $"Ahoj {jmeno}!");
-app.MapGet("/secti/{a}/{b}", (int a, int b) => $"Vysledek {a} + {b} = {a + b}");
-app.MapGet("/nazdarSvete", () => "Nazdar světe!");
->>>>>>> ed7fc6fc23bbc92cd7bbd1c31da4757366c7dce1
-
 app.Run();
